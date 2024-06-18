5f6e19ccfbded8be9f574d374f47e5c72e8e4b28
Noe Osorio <osorionoe@gmail.com>
2024-06-17 20:52:45 -0600

    Add workshops form

---
+++ src/app/workshops/page.tsx
@@ -0,0 +1,12 @@
+export default function WorkshopsPage({
+  params,
+}: {
+  params: { workshop: string };
+}) {
+  return (
+    <div>
+      <div data-tf-live="01J0MKHAMZ2J3VZA4733Q8QY3Y"></div>
+      <script defer src="//embed.typeform.com/next/embed.js"></script>
+    </div>
+  );
+}
