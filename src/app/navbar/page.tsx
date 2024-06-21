a3a1dc60e0f89ef3ea306ebc9d8256433199c86e
EdgarVMora <167935068+EdgarVMora@users.noreply.github.com>
2024-06-21 17:35:55 -0600

    navbar responsive (#3)
    * navbar responsive
    
    * correction navbar responsive
    
    ---------
    
    Co-authored-by: Noe Osorio <noeosorio@MacBook-Air-de-Noe.local>

--- src/app/navbar/page.tsx
+++ src/app/navbar/page.tsx
@@ -1,15 +1,39 @@
-import styles from "./page.module.css"
+"use client";
 
-export default function nav(){
+import styles from "./page.module.css"
+import React, {useState} from "react";
+
+
+export default function Nav(){
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
+
     return(
         <nav className={styles.nav}>
-            <p>Sakura Academy</p>
-            <div className={styles["nav__sections"]}>
-                <a href="/#courses"  className={styles["nav__item"]}>Cursos</a>
-                <a href="/#workshops" className={styles["nav__item"]}>Workshops</a>
-                <a href="/#questions" className={styles["nav__item"]}>Preguntas</a>
-                <a href="/#contact" className={styles["nav__item"]}>Contacto</a>
+            <a href="/#hero" className={styles.nav__brand}> Sakura Academy</a>
+            <div className={styles.nav__toggle} onClick={toggleMenu}>
+            ☰
             </div>
+            <ul className={`${styles.nav__menu} ${isOpen ? styles.active : ""}` }>
+                <li className={styles.nav__item}>
+                    <a href="/#courses" onClick={closeMenu}>Cursos</a>
+                </li>
+                <li className={styles.nav__item}>
+                    <a href="/#workshops" onClick={closeMenu}>Workshop</a>
+                </li>
+                <li className={styles.nav__item}>
+                    <a href="/#questions" onClick={closeMenu}>Preguntas</a>
+                </li>
+                <li className={styles.nav__item}>
+                    <a href="/#contact" onClick={closeMenu}>Contacto</a>
+                </li>
+            </ul>
             
         </nav>
     )
